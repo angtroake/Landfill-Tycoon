d0435c7923054ff4963befb305a8650ead994c14
angtroake <angtroake@mun.ca>
2018-08-11 02:36:08 -0230

    Map Render

---
+++ Map.py
@@ -0,0 +1,95 @@
+from collections import namedtuple
+import csv
+from Constants import *
+import pygame
+import ImageUtil
+
+scrollX = 0
+scrollY = 0
+
+
+MAP = None
+
+
+Zoom = 1
+
+
+
+def loadMap():
+    with open("maps/map1/Map.csv") as csvmap:
+        reader = csv.reader(csvmap)
+        global MAP
+        MAP = list(reader)
+        MAP[0][0] = '0'
+
+
+
+
+def getTileImage(x,y):
+    global MAP
+    val = MAP[x][y]
+    if(val == '0'):
+        return ImageUtil.get_image("water")
+    elif(val == '1'):
+        return ImageUtil.get_image("grass")
+    elif(val == '2'):
+        return ImageUtil.get_image("road")
+    else:
+        return ImageUtil.get_image("temp")
+
+
+
+
+def render(screen):
+    global scrollX
+    global scrollY
+    global Zoom
+    
+    #--------------------------------  TILE RENDERING   -------------------------------------
+
+    for cellY in range(0,MAP_HEIGHT+1):
+        for cellX in range(0,MAP_WIDTH+1):
+            posX = ((cellX * TILE_WIDTH / 2) + (cellY * TILE_WIDTH / 2) - scrollX)*Zoom
+            posY = ((cellY * TILE_HEIGHT / 2) - (cellX * TILE_HEIGHT / 2) - scrollY)*Zoom
+
+            if(posX  > 0 and posX < screen.get_width() - 20 and posY > 0 and posY < screen.get_height()):
+                color = (100,100,100)
+                image = getTileImage(cellX, cellY)
+
+                screen.blit(pygame.transform.scale(image, (int(TILE_WIDTH*Zoom), int(TILE_HEIGHT*Zoom))), (posX, posY-TILE_HEIGHT*Zoom/2))
+                
+                    
+                pygame.draw.line(screen, color, (posX,posY), (posX + TILE_WIDTH*Zoom/2, posY - TILE_HEIGHT*Zoom/2))
+                pygame.draw.line(screen, color, (posX+TILE_WIDTH*Zoom/2, posY-TILE_HEIGHT*Zoom/2), (posX+TILE_WIDTH*Zoom,posY))
+                pygame.draw.line(screen, color, (posX+TILE_WIDTH*Zoom,posY), (posX+TILE_WIDTH*Zoom/2,posY+TILE_HEIGHT*Zoom/2))
+                pygame.draw.line(screen, color, (posX+TILE_WIDTH*Zoom/2,posY+TILE_HEIGHT*Zoom/2),(posX,posY))
+
+    #-------------------------------------------------------------------------------------------
+
+
+
+    #------------------------- MOUSE TILE HOVER ------------------------------------------------
+
+    mousePos = pygame.mouse.get_pos()
+
+    mouseY = int(((mousePos[1]+scrollY)/TILE_HEIGHT) + ((mousePos[0]+scrollX) / TILE_WIDTH))
+    mouseX = -int((-(mousePos[0]+scrollX)/TILE_WIDTH) + ((mousePos[1]+scrollY) / TILE_HEIGHT))
+
+    hoverPosX = ((mouseX * TILE_WIDTH*Zoom / 2) + (mouseY * TILE_WIDTH*Zoom / 2) - scrollX)*Zoom
+    hoverPosY = ((mouseY * TILE_HEIGHT*Zoom / 2) - (mouseX * TILE_HEIGHT*Zoom / 2) - scrollY)*Zoom
+
+    pygame.draw.line(screen, (0,0,255), (hoverPosX,hoverPosY), (hoverPosX + TILE_WIDTH*Zoom/2, hoverPosY - TILE_HEIGHT*Zoom/2))
+    pygame.draw.line(screen, (0,0,255), (hoverPosX+TILE_WIDTH*Zoom/2, hoverPosY-TILE_HEIGHT*Zoom/2), (hoverPosX+TILE_WIDTH*Zoom,hoverPosY))
+    pygame.draw.line(screen, (0,0,255), (hoverPosX+TILE_WIDTH*Zoom,hoverPosY), (hoverPosX+TILE_WIDTH*Zoom/2,hoverPosY+TILE_HEIGHT*Zoom/2))
+    pygame.draw.line(screen, (0,0,255), (hoverPosX+TILE_WIDTH*Zoom/2,hoverPosY+TILE_HEIGHT*Zoom/2),(hoverPosX,hoverPosY))
+
+    #---------------------------------------------------------------------------------------------
+    
+
+
+
+#TODO Adjust render limit when testing finished
+#TODO FIX CURSOR ON ZOOM
+            
+
+            
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 import sys
 import ImageUtil
+import Map
+from pygame.locals import *
 
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((1280, 720), DOUBLEBUF)
+
 done = False
 
 font = pygame.font.Font(None, 30)
 clock = pygame.time.Clock()
 
 ImageUtil.create_image("test", "res/test.png")
+ImageUtil.create_image("grass", "res/tile/tile-grass.png")
+ImageUtil.create_image("road", "res/tile/tile-road.png")
+ImageUtil.create_image("water", "res/tile/tile-water.png")
+ImageUtil.create_image("temp", "res/tile/tile-template.png")
+
+Map.loadMap()
+
+
+Zoom = 1
+ZoomTick = 0
 
 
 while not done:
@@ -19,13 +32,33 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if(event.button == 4):
+                if(Zoom > 0.5):
+                    Zoom -= 0.5
+            elif(event.type == 5):
+                if(Zoom < 2):
+                    Zoom += 0.5
+            print(Zoom)
+            Map.Zoom = Zoom
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        Map.scrollX += 20
+    elif keys[pygame.K_LEFT]:
+        Map.scrollX -= 20
+    if keys[pygame.K_UP]:
+        Map.scrollY -= 20
+    elif keys[pygame.K_DOWN]:
+        Map.scrollY += 20
 
     background = pygame.Surface(screen.get_size())
-    background.fill((255,255,255))
+    background.fill((85,85,85))
     background = background.convert()
     screen.blit(background, (0,0))
 
-    screen.blit(ImageUtil.get_image('test'), (100, 100))
+    Map.render(screen)
 
 
 
