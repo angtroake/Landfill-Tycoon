6722b2230d5a4dc9140b9727926f3a78cf4b1f51
sirjayjayec <jackelliott7796@gmail.com>
2018-08-12 15:31:31 +0100

    fucking with shit

--- City.py
+++ City.py
@@ -8,28 +8,39 @@ global seltileX
 global seltileY
 surTiles = [None] * 100
 def getSurroundingtiles(X,Y):
-        surTiles = [None] * 100
-        #North
-        surTiles[0]  = X+1
-        surTiles[1]  = Y
-        surTiles[2]  = Map.GMAP[surTiles[0]][surTiles[1]]
-        #East
-        surTiles[10] = X
-        surTiles[11] = Y+1
-        surTiles[12]  = Map.GMAP[surTiles[10]][surTiles[11]]
-        #South
-        surTiles[20] = X-1
-        surTiles[21] = Y
-        surTiles[22]  = Map.GMAP[surTiles[20]][surTiles[21]]
-        #West
-        surTiles[30] = X
-        surTiles[31] = Y-1
-        surTiles[32]  = Map.GMAP[surTiles[30]][surTiles[31]]
-        return surTiles
+    surTiles = [None] * 100
+    #North
+    surTiles[0]  = X+1
+    surTiles[1]  = Y
+    surTiles[2]  = Map.GMAP[surTiles[0]][surTiles[1]]
+    #East
+    surTiles[10] = X
+    surTiles[11] = Y+1
+    surTiles[12]  = Map.GMAP[surTiles[10]][surTiles[11]]
+    #South
+    surTiles[20] = X-1
+    surTiles[21] = Y
+    surTiles[22]  = Map.GMAP[surTiles[20]][surTiles[21]]
+    #West
+    surTiles[30] = X
+    surTiles[31] = Y-1
+    surTiles[32]  = Map.GMAP[surTiles[30]][surTiles[31]]
+    return surTiles
+
+def isasurroundingtilex(tileX,tileY, x):
+    tiles = []
+    tiles = getSurroundingtiles(tileX, tileY)
+    for i in range(0,3):
+        if(tiles[i*10+2] == x):
+            return True
+    return False
+
+
+
 def maptest():
     btilesX = []
     btilesY = []
-    #x and y of all tiles surrounding selected built tile  
+    #x and y of all tiles surrounding selected built tile
     surtilesX = []
     surtilesY = []
     surtilesV = []
@@ -38,7 +49,7 @@ def maptest():
     #find all built tiles and put into an array
     for cellY in range(0,MAP_HEIGHT+1):
         for cellX in range(0,MAP_WIDTH+1):
-            if(Map.GMAP[cellX][cellY] == "0"):
+            if(int(Map.MAP[cellX][cellY]) >= 10 and int(Map.MAP[cellX][cellY]) <= 20):
                 btilesX.append(cellX)
                 btilesY.append(cellY)
 
@@ -63,14 +74,14 @@ def maptest():
         newtileX = newtile*10+0
         newtileY = newtile*10+1
         if(surtiles[newtile*10+2] != "00" and surtiles[newtile*10+2] !="0" and surtiles[newtile*10+2] !="99"):
-            if(Map.MAP[surtiles[newtileX]][surtiles[newtileY]] == "1"):
+            print(str(isasurroundingtilex(surtiles[newtileX],surtiles[newtileY],"20")))
+            if(Map.MAP[surtiles[newtileX]][surtiles[newtileY]] == "1"): #if grass change to building
                 Map.MAP[surtiles[newtileX]][surtiles[newtileY]] = str(10+random.randint(0,2))
                 Map.GMAP[surtiles[newtileX]][surtiles[newtileY]] = "0"
-            elif(Map.MAP[surtiles[newtileX]][surtiles[newtileY]] == "2"):
+            elif(Map.MAP[surtiles[newtileX]][surtiles[newtileY]] == "2" and isasurroundingtilex(surtiles[newtileX],surtiles[newtileY],"20")): #if road to be, change to road#
+                print("sel tile" + str(seltileX) + " " + str(seltileY))
+                print("Random sur tile" + str(surtiles[newtileX]) + " " + str(surtiles[newtileY]))
                 Map.MAP[surtiles[newtileX]][surtiles[newtileY]] = "20"
-                Map.GMAP[surtiles[newtileX]][surtiles[newtileY]] = "0"
-            else:
-                Map.MAP[surtiles[newtileX]][surtiles[newtileY]] = "3"
                 Map.GMAP[surtiles[newtileX]][surtiles[newtileY]] = "0"
     surtilesInvalid = 0
     #put all tiles avalible to be built on into array
